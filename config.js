ad2f545f0c95180daaeda3126697bcdc8081dbbb
joaopfsilva <joaopfsilva@joaopfsilva-pc.local>
2017-08-25 10:34:27 +0200

    first commit. adding default project structure

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAFA5Jjj8mkBAAZAqxg2un1YZAtTuCMDZCby3wb0BC427cwylTgd94lWdIXjE8g5gRKeebxJh22K5ZAlJdn7ZCPeCJjBbRqmYgjUkq4LX4j0noiwRlO6FCAr8LxjwrquNSEtqpWaUhqczxlqGhR0yE5x1HeRi68e8laFAfHqtfQZDZD',
+    FB_VERIFY_TOKEN: 'techno_oracle_chabot_verify_token_101',
+    API_AI_CLIENT_ACCESS_TOKEN: '1f3bacb55e1e4e9c84b50a671e408f3b',
+    FB_APP_SECRET: '6254236d691cbe4a8ad78274a6daac27',
+    SERVER_URL: "https://techno-oracle-chabot.herokuapp.com/",
+};
